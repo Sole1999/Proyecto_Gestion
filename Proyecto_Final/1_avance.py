c814fb5c9b71a9aa9a72740f0b66fb6f829124fb
Sole1999 <141879281+Sole1999@users.noreply.github.com>
2024-07-18 19:30:03 -0500

    Update 1_avance.py
    Modificado el 1 avance

--- Proyecto_Final/1_avance.py
+++ Proyecto_Final/1_avance.py
@@ -4,10 +4,10 @@ import numpy as np
 import seaborn as sns
 from pandas.plotting import scatter_matrix
 
-filename = 'dataset/amazon.csv'
+filename = 'dataset/nearest-earth-objects(1910-2024).csv'
 data = pd.read_csv(filename)
 
-
+numeric_data = data.select_dtypes(include=['number'])
 
 #Imprimir todos los datos
 def print_all_data():
@@ -39,43 +39,50 @@ def summary():
 
 
 def correlation():
-    correlation_matrix = numeric_data.corr()
-    numeric_data = data.select_dtypes(include=['float64', 'int64'])
-    sns.heatmap(correlation_matrix, vmax=1, square=True, annot=True, cmap='viridis')
-    print(sns.heatmap)
+    
+    
+    correlations = numeric_data.corr(method='pearson')
+    print(correlations)
 
 
 def skew(): #sesgo
-    a = data.skew()
+    a = numeric_data.skew()
     print(a)
     
 
 
 def densidad_matriz():
 #Código básico para graficar
-    fig = plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(6,6))
     ax = fig.gca()
     #Código para graficar una matriz de dispersión
-    data.plot(ax=ax, kind='density', subplots=True, layout=(4,4), sharex=False)
+    numeric_data.plot(ax=ax, kind='density', subplots=True, layout=(3,3), sharex=False)
     plt.show()
 
 
 def Matriz_del_diagrama_de_caja():
     #Código básico para graficar
-    fig = plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(12,12))
     ax = fig.gca()
     #Código para graficar una matriz de un diagrama de caja
-    data.plot(ax=ax, kind='box', subplots=True, layout=(4,4), sharex=False)
+    data.plot(ax=ax, kind='box', subplots=True, layout=(6,6), sharex=False)
     plt.show()
   
-
+# Crear el box plot para 'price' por categoría de producto, por ejemplo 'product_category'
+    #plt.figure(figsize=(12, 12))
+    #sns.boxplot(x='category', y='actual_price', data=data)
+    #plt.title('Box Plot of Price by Product Category')
+    #plt.xlabel('Product Category')
+    #plt.ylabel('Price')
+    #plt.xticks(rotation=90)
+    #plt.show()
 
 def Matriz_de_correlacion():
     #correlations = data.corr(method='pearson')
     plt.figure(figsize=(12,10))
     plt.title('Matriz de correlacion')
     # Seleccionar columnas numéricas para la matriz de correlación
-    numeric_data = data.select_dtypes(include=['float64', 'int64'])
+    numeric_data = data.select_dtypes(include=['float64', 'int64', 'string'])
     correlation_matrix = numeric_data.corr()
     sns.heatmap(correlation_matrix, vmax=1, square=True, annot=True, cmap='viridis')
     plt.show()
@@ -96,9 +103,13 @@ def Matriz_de_dispersion():
 #print_data_types()
 #count_data_by_class()
 #summary()
-
 #correlation()
 #skew()
+
+#densidad_matriz()
+#Matriz_del_diagrama_de_caja()
+#Matriz_de_correlacion()
+Matriz_de_dispersion()
 
 densidad_matriz()
 #Matriz_del_diagrama_de_caja()
